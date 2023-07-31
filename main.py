b83bb5fc736da12654cc4226f3d843629daa864a
Rome2005 <132018425+Rome2005@users.noreply.github.com>
2023-07-31 00:27:08 +0000

    This is my main gui

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-from tkinter import *
-from maingui import MainGUI
-
-# main routine
-if __name__ == "__main__":
-    root = Tk()
-    root.title("Math Program")
-    maingui = MainGUI()
-    root.mainloop()
-
+print(hello world)
 
