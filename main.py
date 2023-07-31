b4814acbccb88979d6ff8a171abb577a6a2008f6
Rome2005 <132018425+Rome2005@users.noreply.github.com>
2023-07-31 00:28:05 +0000

    MainGUIv1

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print(hello world)
+from tkinter import *
+from maingui import MainGUI
 
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Math Program")
+    maingui = MainGUI()
+    root.mainloop()
